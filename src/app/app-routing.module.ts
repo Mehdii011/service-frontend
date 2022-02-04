4498c7e1dea809231445a2177035affc269556ea
elakhlif <el-mehdi.lakhlifi@capgemini.com>
2022-02-04 14:35:00 +0100

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,54 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddClientComponent } from './componenets/add-client/add-client.component';
+import { AddCompteComponent } from './componenets/add-compte/add-compte.component';
+import { AddConseillerComponent } from './componenets/add-conseiller/add-conseiller.component';
+import { DetailClientComponent } from './componenets/detail-client/detail-client.component';
+import { DetailCompteComponent } from './componenets/detail-compte/detail-compte.component';
+import { DetailConseillerComponent } from './componenets/detail-conseiller/detail-conseiller.component';
+import { ListClientComponent } from './componenets/list-client/list-client.component';
+import { ListCompteComponent } from './componenets/list-compte/list-compte.component';
+import { ListConseillerComponent } from './componenets/list-conseiller/list-conseiller.component';
+import { LoginComponent } from './componenets/login/login.component';
+import { PageNotFoundComponent } from './componenets/page-not-found/page-not-found.component';
+import { RegisterComponent } from './componenets/register/register.component';
+import { RetraitComponent } from './componenets/retrait/retrait.component';
+import { UpdateClientComponent } from './componenets/update-client/update-client.component';
+import { UpdateCompteComponent } from './componenets/update-compte/update-compte.component';
+import { UpdateConseillerComponent } from './componenets/update-conseiller/update-conseiller.component';
+import { VersementComponent } from './componenets/versement/versement.component';
+import { VirementComponent } from './componenets/virement/virement.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",redirectTo:'/clients',pathMatch:'full'},
+  {path:"clients",children:[
+    {path:"",component:ListClientComponent},
+    {path:"edit/:id",component:UpdateClientComponent},
+    {path:"add",component:AddClientComponent},
+    {path:"detail/:id",component:DetailClientComponent},
+  ]},
+  {path:"conseiller",children:[
+    {path:"",component:ListConseillerComponent},
+    {path:"edit/:id",component:UpdateConseillerComponent},
+    {path:"add",component:AddConseillerComponent},
+    {path:"detail/:id",component:DetailConseillerComponent},
+  ]},
+  {path:"compte",children:[
+    {path:"",component:ListCompteComponent},
+    {path:"edit/:id",component:UpdateCompteComponent},
+    {path:"add",component:AddCompteComponent},
+    {path:"detail/:id",component:DetailCompteComponent},
+  ]},
+  {path:"operation",children:[
+    {path:"virement",component:VirementComponent},
+    {path:"veresement",component:VersementComponent},
+    {path:"retrait",component:RetraitComponent},
+  ]},
+  {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'**',component:PageNotFoundComponent}
+
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,59 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componenets/login/login.component';
+import { RegisterComponent } from './componenets/register/register.component';
+import { NavbarComponent } from './componenets/navbar/navbar.component';
+import { ListClientComponent } from './componenets/list-client/list-client.component';
+import { ListConseillerComponent } from './componenets/list-conseiller/list-conseiller.component';
+import { AddClientComponent } from './componenets/add-client/add-client.component';
+import { AddConseillerComponent } from './componenets/add-conseiller/add-conseiller.component';
+import { UpdateClientComponent } from './componenets/update-client/update-client.component';
+import { UpdateConseillerComponent } from './componenets/update-conseiller/update-conseiller.component';
+import { ListCompteComponent } from './componenets/list-compte/list-compte.component';
+import { OperationComponent } from './componenets/operation/operation.component';
+import { VirementComponent } from './componenets/virement/virement.component';
+import { VersementComponent } from './componenets/versement/versement.component';
+import { RetraitComponent } from './componenets/retrait/retrait.component';
+import { PageNotFoundComponent } from './componenets/page-not-found/page-not-found.component';
+import { DetailClientComponent } from './componenets/detail-client/detail-client.component';
+import { DetailConseillerComponent } from './componenets/detail-conseiller/detail-conseiller.component';
+import { DetailCompteComponent } from './componenets/detail-compte/detail-compte.component';
+import { UpdateCompteComponent } from './componenets/update-compte/update-compte.component';
+import { AddCompteComponent } from './componenets/add-compte/add-compte.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent,
+    ListClientComponent,
+    ListConseillerComponent,
+    AddClientComponent,
+    AddConseillerComponent,
+    UpdateClientComponent,
+    UpdateConseillerComponent,
+    ListCompteComponent,
+    OperationComponent,
+    VirementComponent,
+    VersementComponent,
+    RetraitComponent,
+    PageNotFoundComponent,
+    DetailClientComponent,
+    DetailConseillerComponent,
+    DetailCompteComponent,
+    UpdateCompteComponent,
+    AddCompteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
